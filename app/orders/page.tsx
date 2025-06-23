1b80ba234a0ebe1139f42f622080818da9b8a2c9
mbjelobrk1 <148221673+mbjelobrk1@users.noreply.github.com>
2025-06-23 13:19:55 +0200

    Fix

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -1,5 +1,4 @@
 export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
 import { supabase } from '../../lib/supabaseClient';
 import OrdersDataGrid from '../../components/OrdersDataGrid';
 
